f6e165d0834965f22e5dcb50fd36581109bb8820
Leslie Cheung <l38cheun@scs.ryerson.ca>
2017-07-15 10:00:03 -0400

    Add VirtualPetEvents.ino for timed actions

---
+++ VirtualPetEvents.ino
@@ -0,0 +1,27 @@
+unsigned long prevMove = 0;
+unsigned long moveDelay = 700;
+int timesMoved = 0;
+
+void timePetMovements(unsigned long currentTime) {
+  if(currentTime - prevMove >= moveDelay) {
+    prevMove = currentTime;
+    if(timesMoved < 3) {
+      lcd.scrollDisplayLeft();
+      timesMoved++;
+    }
+    else if(timesMoved < 6) {
+      lcd.scrollDisplayRight();
+      timesMoved++;
+    }
+    else { timesMoved = 0; }
+  }
+}
+
+void timePetPooping(unsigned long currentTime) {
+  if(currentTime % 30000 == 0 && !pooped) {
+    drawPet(FROWNING);
+    drawPoop();
+    pooped = true;
+    playSound(poopTone,quarterAndEighthNotes);
+  }
+}
